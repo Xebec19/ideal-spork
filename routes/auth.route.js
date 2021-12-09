e8af9449cd704271b07e31f8d04a1e594bc088a6
xebec19 <rohant946@gmail.com>
2021-12-09 21:20:39 +0530

    restart

--- routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const route = express.Router();
-
-route.get("/", (req, res, next) => {
-  // res.render("login");
-  // res.redirect("/view/login");
-});
-
-module.exports = route;
